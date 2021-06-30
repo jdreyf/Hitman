418c581d81b026900dd2cad8dc463b4a32fe5fde
jonathan dreyfuss <jdreyf@gmail.com>
2021-06-29 22:31:00 -0400

    Update test-sim_omics.R

--- tests/testthat/test-sim_omics.R
+++ tests/testthat/test-sim_omics.R
@@ -1,9 +1,34 @@
 context("sim-omics")
 
-test_that("sims match", {
+test_that("compare sims sc1", {
   prop.sig.hit <- Hitman::sim_barfield(med.fnm = "hitman", b1t2=0.39, nsim = 10, ngene=9, nsamp = 50, t1=0.59)
-  # FDR=0.5 if no other genes affect top FDR; otherwise, FDR will be lower
-  sc1 <- Hitman::sim_omics(nsamp=50, ngene=10, FDR=0.5, prop.consistent=1/10, b1t2 = 0.39, t1=0.59, nsim=10,
-                            Sigma = diag(10), prop.1c = 0, seed=0)
+  # FDR=0.5 if no other genes affect top FDR; otherwise, FDR will be lower, so have more power
+  ngene <- 10
+  sc1 <- Hitman::sim_omics(nsamp=50, ngene=ngene, FDR=0.5, prop.consistent=1/ngene, b1t2 = 0.39, t1=0.59, nsim=10,
+                            Sigma = diag(ngene), prop.1c = 0, seed=0)
   expect_gte(sc1["hitman", "power"], prop.sig.hit)
+
+  ngene <- 20
+
+  # t1 < 0 --> mediators should not be significant
+  sc1.negt1 <- Hitman::sim_omics(nsamp=50, ngene=ngene, FDR=0.5, prop.consistent=1/ngene, b1t2 = 0.39, t1 = -5, nsim=10,
+                           Sigma = diag(ngene), prop.1c = 0, seed=0)
+  expect_lte(sc1.negt1["hitman", "power"], 0)
+  expect_lte(sc1.negt1["lotman", "power"], 0)
+  expect_gt(sc1.negt1["js", "power"], 0)
+
+  # FDR effect
+  sc1.fdr15 <- Hitman::sim_omics(nsamp=50, ngene=ngene, FDR=0.15, prop.consistent=1/ngene, b1t2 = 0.39, t1=0.59, nsim=10,
+                                      Sigma = diag(ngene), prop.1c = 0, seed=0)
+  expect_true(all(sc1.fdr15$n.hits <= sc1$n.hits))
+  expect_true(all(sc1.fdr15$power <= sc1$power))
+
+  # b1t2
+  sc1.lob1t2 <- Hitman::sim_omics(nsamp=50, ngene=ngene, FDR=0.5, prop.consistent=1/ngene, b1t2 = 0.39, t1=2, nsim=10,
+                           Sigma = diag(ngene), prop.1c = 0, seed=0)
+  sc1.hib1t2 <- Hitman::sim_omics(nsamp=50, ngene=ngene, FDR=0.5, prop.consistent=1/ngene, b1t2 = 1, t1=2, nsim=10,
+                                  Sigma = diag(ngene), prop.1c = 0, seed=0)
+  expect_true(all(sc1.hib1t2$power >= sc1.lob1t2$power))
+
+  # prop but don't know effect
 })
